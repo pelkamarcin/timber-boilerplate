4e499c3f5999a65652598ae9d855b352e0d8e3b2
Marcin Pełka <palkens1989@gmail.com>
2024-01-03 15:49:49 +0100

    Refactor

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,76 @@
 import dotenv from 'dotenv';
 import {defineConfig} from 'vite';
 import sassGlobImports from 'vite-plugin-sass-glob-import';
+import * as sass from 'sass';
+import * as fs from 'fs';
+import {glob} from 'glob';
+import * as path from 'path';
+
 
 dotenv.config();
 
 export default defineConfig({
-    publicDir: 'resources/static',
+    publicDir: 'resources/public',
+    css: {
+        devSourcemap: true,
+    },
     build: {
+        sourcemap: 'hidden',
         assetsDir: '',
         emptyOutDir: true,
         manifest: true,
         outDir: `dist`,
         rollupOptions: {
             input: 'resources/js/index.js',
+
         },
     },
     plugins: [
         sassGlobImports(),
         {
-            name: 'php',
-            handleHotUpdate( {file, server} ) {
-                if (file.endsWith( '.php' ) || file.endsWith( '.twig' )) {
-                    server.ws.send( {type: 'full-reload', path: '*'} );
+            name: 'blocks',
+            async handleHotUpdate({file, server}) {
+                if (file.indexOf('resources/scss/blocks') > 0) {
+                    await compileBlocks();
+                    server.ws.send({type: 'full-reload', path: '*'});
                 }
             },
-    },
+            async buildEnd() {
+                compileBlocks();
+            }
+        },
+        {
+            name: 'php',
+            handleHotUpdate({file, server}) {
+                if (file.endsWith('.php') || file.endsWith('.twig')) {
+                    server.ws.send({type: 'full-reload', path: '*'});
+                }
+            },
+        },
     ],
 });
+
+async function compileBlocks() {
+    const blocksFiles = await glob('resources/scss/blocks/*.scss');
+    return new Promise((resolve, reject) => {
+        const noOfFiles = blocksFiles.length;
+        let savedFilesNo = 0;
+        blocksFiles.forEach((item) => {
+            const name = path.parse(item).name;
+            const result = sass.compile(item,
+                {
+                    loadPaths: [
+                        './node_modules'
+                    ]
+                });
+            fs.writeFile(`src/Blocks/${name}/style.css`, result.css, () => {
+                savedFilesNo++;
+                if (savedFilesNo >= noOfFiles) {
+                    resolve();
+                }
+            });
+        });
+    });
+
+
+}
